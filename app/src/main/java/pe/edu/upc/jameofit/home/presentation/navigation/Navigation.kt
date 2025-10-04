e23692c349f9714615958fee0071432b0c74ce70
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-10-03 20:30:10 -0500

    chore(navigation): reorder auth flow and prevent blank screen on login

--- app/src/main/java/pe/edu/upc/jameofit/home/presentation/navigation/Navigation.kt
+++ app/src/main/java/pe/edu/upc/jameofit/home/presentation/navigation/Navigation.kt
@@ -1,8 +1,10 @@
 package pe.edu.upc.jameofit.home.presentation.navigation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import pe.edu.upc.jameofit.home.presentation.view.Home
 
 @Composable
-fun Navigation() {
-
+fun Navigation(goToLogin: () -> Unit = {}) {
+    Text("Text Loaded")
 }
