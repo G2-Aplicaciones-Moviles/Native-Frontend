b67ef58afd5a60ebd2dc8ef1521b507052c9db1f
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-10-07 21:50:55 -0500

    fix(RecommendationsService)

--- app/src/main/java/pe/edu/upc/jameofit/recommendations/data/remote/RecommendationsService.kt
+++ app/src/main/java/pe/edu/upc/jameofit/recommendations/data/remote/RecommendationsService.kt
@@ -6,8 +6,12 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface RecommendationsService {
+
     @GET("recommendations/user/{userId}")
     suspend fun getRecommendationsByUser(
         @Path("userId") userId: Long
     ): Response<List<RecommendationResponse>>
+
+    @GET("recommendations")
+    suspend fun getAllRecommendations(): Response<List<RecommendationResponse>>
 }
